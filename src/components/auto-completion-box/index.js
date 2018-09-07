2685d24f00912addd290479329e270e462d449c3
Victoto <victoria_yang@outlook.com>
2018-09-07 01:35:31 -0400

    add auto-completion box directory

---
+++ src/components/auto-completion-box/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class AutoCompletionBox extends React.Component {
+  render() {
+    return <div>something for the auto-completion project</div>;
+  }
+}
+
+export default AutoCompletionBox;
