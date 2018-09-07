a85c89eb2a6c4f8bff6231f39345cc424e4c110a
Victoto <victoria_yang@outlook.com>
2018-09-07 15:09:35 -0400

    update UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 
 // components
 import AutoCompletionBox from "./components/auto-completion-box";
--- src/components/auto-completion-box/index.js
+++ src/components/auto-completion-box/index.js
@@ -3,10 +3,33 @@ import style from "./auto-completion-box.css";
 
 class AutoCompletionBox extends React.Component {
   render() {
-    return (
-      <div className={style.box}>
-        <h1>Auto-completion Test Box</h1>
+    const textBoxOption = (
+      <div>
         <textarea className={style.textArea} />
+      </div>
+    );
+
+    const seperateBoxOption = (
+      <div className={style.seperateBoxOption}>
+        <div className={style.boxRow}>
+          <label>SELECT</label>
+          <input className={style.input} />
+        </div>
+
+        <div className={style.boxRow}>
+          <label>WHERE</label>
+          <input className={style.input} />
+          <input className={style.input} />
+          <input className={style.input} />
+        </div>
+      </div>
+    );
+
+    return (
+      <div className={style.page}>
+        <h1>Auto-completion Test Box</h1>
+        {textBoxOption}
+        {seperateBoxOption}
       </div>
     );
   }
