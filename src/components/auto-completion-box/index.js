9c3622675fe30f6d291e34a6cb7209fa50ff7a6b
Victoto <victoria_yang@outlook.com>
2018-09-08 01:35:40 -0400

    remove boxes

--- src/components/auto-completion-box/index.js
+++ src/components/auto-completion-box/index.js
@@ -9,27 +9,10 @@ class AutoCompletionBox extends React.Component {
       </div>
     );
 
-    const seperateBoxOption = (
-      <div className={style.seperateBoxOption}>
-        <div className={style.boxRow}>
-          <label>SELECT</label>
-          <input className={style.input} />
-        </div>
-
-        <div className={style.boxRow}>
-          <label>WHERE</label>
-          <input className={style.input} />
-          <input className={style.input} />
-          <input className={style.input} />
-        </div>
-      </div>
-    );
-
     return (
       <div className={style.page}>
         <h1>Auto-completion Test Box</h1>
         {textBoxOption}
-        {seperateBoxOption}
       </div>
     );
   }
